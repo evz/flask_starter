64f9c9cc1469fbfaa0b84108d1ca95e156dfda42
Eric van Zanten <eric.vanzanten@gmail.com>
2015-07-28 20:52:39 -0500

    A couple mods

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(request.args)
     print(request.form)
-    return render_template('index.html')
+    message = ''
+    if request.method == 'POST':
+        filename = request.files['input_file'].filename
+        now = datetime.now().isoformat()
+        full_name = '%s_%s_%s' % (filename, request.form['name'], now)
+        with open(full_name, 'w') as f:
+            f.write(request.files['input_file'].read())
+        # message = 'Uplaod successful!'
+        return redirect(url_for('next_page'))
+    return render_template('index.html', message=message)
+
+@app.route('/next-page/')
+def next_page():
+    return 'woo!'
 
 if __name__ == "__main__":
     app.run(debug=True)
